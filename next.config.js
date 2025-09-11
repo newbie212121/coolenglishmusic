653bd7e82720434cd1bbf13b9df3dc7f876b48a4
newbie212121 <150523095+newbie212121@users.noreply.github.com>
2025-09-10 22:06:09 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['d1uqdf1080xgw5.cloudfront.net'],
+  },
+};
+
+module.exports = nextConfig;
