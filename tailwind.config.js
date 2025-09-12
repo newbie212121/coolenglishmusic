471d1a6ac3fdd695a15c82ad9f2a9d0590fe841c
newbie212121 <150523095+newbie212121@users.noreply.github.com>
2025-09-11 22:16:33 -0400

    fix: downgrade dependencies for stability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
