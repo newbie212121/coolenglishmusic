9540dbd06b48f8a0784b87295500dfa337eec3d4
newbie212121 <150523095+newbie212121@users.noreply.github.com>
2025-09-18 09:13:57 -0400

    Your commit message here

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
-import amplifyconfig from "../src/amplifyconfiguration.json";
+import amplifyconfig from '../amplifyconfiguration.json';
 import { AuthProvider } from "@/context/AuthContext";
 
 Amplify.configure(amplifyconfig);
